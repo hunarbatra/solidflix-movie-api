0e0f426341dcdedd96d0c7a9b91dc29025811fe6
Hunar Batra <i@hunarbatra.com>
2022-11-13 13:42:08 +0530

    added content based recommendation script

--- app.py
+++ app.py
@@ -1,41 +1,22 @@
 from flask import Flask, request, jsonify
-# from flask_ngrok import run_with_ngrok 
+# from flask_ngrok import run_with_ngrok # uncomment to use ngrok
 from flask_cors import CORS
 
-from tmdbv3api import TMDb
-from tmdbv3api import Movie
-
-import random
+import recommendation
 
 app = Flask(__name__)
 CORS(app)
 
-# tmdb setup
-tmdb = TMDb()
-tmdb.api_key = 'e70f4a66202d9b5df3586802586bc7d2'
-tmdb.language = 'en'
-tmdb.debug = True
-movie = Movie()
-
-# run_with_ngrok(app) # comment
+# run_with_ngrok(app) # uncomment to use ngrok
   
 @app.route("/", methods=['POST'])
 def recommendation():
     movies = request.get_json()
-    print(movies)
+    res = recommendation.results(movies)
+    print(res)
+    return jsonify(res)
+    
+# app.run() 
 
-    movieIDs, recommendedMovies = [], []
-    for m in movies:
-        search = movie.search(m)
-        for res in search:
-            movieIDs.append(res.id)
-    for id in movieIDs:
-        recos = movie.recommendations(id)
-        for reco in recos:
-            recommendedMovies.append(reco.title)
-    sampledRecos = random.sample(recommendedMovies, 5)
-
-    return jsonify(sampledRecos)
-
-if __name__=='__main__': 
-    app.run(port = 5000, debug = True)
+if __name__=='__main__': # uncomment
+    app.run()
